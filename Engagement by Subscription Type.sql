caabfa31604397cb187ff4fda3cf0d1a1a73a65e
P Pegu <ppegu2935@gmail.com>
2025-09-23 15:24:08 +0530

    Delete Engagement by Subscription Type.sql

--- Engagement by Subscription Type.sql
+++
@@ -1,7 +0,0 @@
--- Engagement by Subscription Type
-
-SELECT Subscription_Type,
-       ROUND(AVG(Engagement_Score),2) AS avg_engagement,
-       COUNT(user_id) AS user_count
-FROM spotify
-GROUP BY Subscription_Type;
